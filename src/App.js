dea2a074d2c3cff81d36df1ef2612dbae41b1eca
Shane Geary <shane.geary@glowstik.com>
2022-03-30 11:59:46 -0600

    Initial commit and structure for viewport testing

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <input type='text'>Input...</input>
+        <footer>
+
+        </footer>
+      </div>
+    </div>
+  );
+}
+
+export default App;
